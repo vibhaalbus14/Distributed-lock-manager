21310d90308879f8cc705f6c08f5e98b84266417
vibhaalbus14 <vibhaa.ha@gmail.com>
2026-05-21 22:28:29 +0530

    initializing mandatory buffer channels and calling it in main

---
+++ internal/startup/initialize.go
@@ -0,0 +1,40 @@
+package startup
+
+import (
+	"distributed_lock_manager/internal/manager"
+	"distributed_lock_manager/internal/network"
+	"distributed_lock_manager/internal/protocol"
+	"os"
+	"time"
+)
+
+var (
+
+	// Global channel to signal main.go to shut down
+	ShutdownChan     chan os.Signal
+	networkSimulator *network.NetworkSimulator
+	lockManager      *manager.LockManager
+)
+
+func Initialize() {
+	ShutdownChan = make(chan os.Signal, 1)
+	fromManagerChannel := make(chan protocol.Message, 250)
+	toManagerChannel := make(chan protocol.Message, 250)
+
+	//first create network
+	networkSimulator = network.NewNetworkSimulator(toManagerChannel)
+	//create lock manager
+	lockManager = manager.NewLockManager(5*time.Second, fromManagerChannel)
+
+	//now start listeing loops in both nw and manager
+	go networkSimulator.StartPipeline()
+	go networkSimulator.ReturnPipeline(fromManagerChannel)
+	go lockManager.Start(toManagerChannel)
+}
+
+func PointerToNS() *network.NetworkSimulator {
+	return networkSimulator
+}
+func PointerToLM() *manager.LockManager {
+	return lockManager
+}
