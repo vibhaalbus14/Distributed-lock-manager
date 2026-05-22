e082fc69d1c95fca86c0aba3e97f15d59c9f4ae5
vibhaalbus14 <vibhaa.ha@gmail.com>
2026-05-22 17:20:18 +0530

    added new endpoint to restart node from crashed

---
+++ internal/handlers/node_restart.go
@@ -0,0 +1,36 @@
+package handlers
+
+import (
+	cluster_manager "distributed_lock_manager/internal/nodeClusterManager"
+	"net/http"
+
+	"github.com/gin-gonic/gin"
+)
+
+func Node_restart(ctx *gin.Context) {
+
+	var v Val
+	err := ctx.ShouldBindJSON(&v)
+	if err != nil {
+		ctx.JSON(http.StatusBadRequest, gin.H{"res": "please provide id in string format"})
+	}
+	nodeId := v.Id
+
+	// Look up the live memory pointer in your cluster map
+	n, exists := cluster_manager.HmNodes[nodeId]
+
+	// Gracefully handle a missing node instead of crashing the server
+	if !exists || n == nil {
+		ctx.JSON(http.StatusNotFound, gin.H{
+			"error": "Node execution context not found or already deleted",
+		})
+		return
+	}
+
+	n.Restart()
+
+	ctx.JSON(http.StatusOK, gin.H{
+		"message": " Request sent to nw",
+	})
+
+}
